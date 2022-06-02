ae4967bfcd5c856bade6bb957946e367e371963a
Mikko Saarela <Mikko.Saarela@aristocrat.com>
2022-06-02 09:30:18 +0300

    Original problem report version

---
+++ MultipleStartupProjects/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultipleStartupProjects.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
